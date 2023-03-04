4ab5139d23af577caec9959dd640fd6959a9380b
VadimZhukov <vadim.zhukov@gmail.com>
2023-03-05 00:01:24 +0300

    file attachement when creating a question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,7 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, dependent: :destroy
 
+  has_one_attached :file
+
   validates :title, :body, presence: true
 end
--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -9,33 +9,47 @@ feature "create answer on question page", "
   given!(:question) { create(:question) }
   given!(:answers) { create_list(:answer, 3, question: question) }
 
-  background do
-    login(user)
-    visit question_path(question)
-  end
+  
 
-  scenario "create answer with valid body", js: true do
-    fill_in "answer_body", with: "Test answer body"
-    click_on "Submit answer"
+  context "A11d user" do
+    background do
+      login(user)
+      visit question_path(question)
+    end
 
-    expect(page).to have_content question.title
-    expect(page).to have_content question.body
+    scenario "create answer with valid body", js: true do
+      fill_in "answer_body", with: "Test answer body"
+      click_on "Submit answer"
 
-    answers.each { |a| expect(page).to have_content a.body }
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body
 
-    within ".answers-list" do  
-      expect(page).to have_content "Test answer body"
+      answers.each { |a| expect(page).to have_content a.body }
+
+      within ".answers-list" do  
+        expect(page).to have_content "Test answer body"
+      end
+    end
+
+    scenario "create answer with invalid empty body", js: true do
+      click_on "Submit answer"
+
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body
+
+      within ".answer-errors" do
+        expect(page).to have_content "Body can't be blank"
+      end
     end
   end
 
-  scenario "create answer with invalid empty body", js: true do
-    click_on "Submit answer"
+  context "Una11d user" do
+    scenario "Can not create answer", js: true do
+      visit question_path(question)
 
-    expect(page).to have_content question.title
-    expect(page).to have_content question.body
-
-    within ".answer-errors" do
-      expect(page).to have_content "Body can't be blank"
+      expect(page).not_to have_content "New answer"
+      expect(page).not_to have_button "Submit answer"
     end
+
   end
 end
